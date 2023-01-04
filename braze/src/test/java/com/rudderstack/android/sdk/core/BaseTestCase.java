47a2a560803fe58ef81682bc212b52ff222fb09a
Abhishek Pandey <abhishekpandey10196@gmail.com>
2023-01-04 19:57:23 +0530

    fix: unit test in braze module

--- braze/src/test/java/com/rudderstack/android/sdk/core/BaseTestCase.java
+++ braze/src/test/java/com/rudderstack/android/sdk/core/BaseTestCase.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import androidx.test.core.app.ApplicationProvider;
 
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
@@ -20,5 +21,10 @@ public class BaseTestCase {
         Context context = ApplicationProvider.getApplicationContext();
         rudderClient = RudderClient.getInstance(context, TEST_WRITE_KEY, new RudderConfig.Builder().withDebug(true));
         Thread.sleep(1000);
+    }
+
+    @Test
+    public void test() {
+
     }
 }
