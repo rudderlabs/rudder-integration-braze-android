f7a3cc88a1eb6d6dde7c84e316d6127d4be5d6a3
Abhishek Pandey <64667840+1abhishekpandey@users.noreply.github.com>
2025-04-24 14:32:56 +0530

    fix: merging of anonymous with identified profile (#33)
    * feat: update braze sdk version to 35.0.0
    
    * chore: remove api key and data plane url
    
    * fix: merging of anonymous with identified profile
    
    This is done by setting the Braze alias ID after it's SDK is initialised.
    
    * chore: empty commit to trigger github action
    
    ---------
    
    Co-authored-by: Vishal Gupta <vguptakota1998@gmail.com>

--- braze/src/main/java/com/rudderstack/android/integration/braze/BrazeIntegrationFactory.java
+++ braze/src/main/java/com/rudderstack/android/integration/braze/BrazeIntegrationFactory.java
@@ -133,6 +133,8 @@ public class BrazeIntegrationFactory extends RudderIntegration<Braze> {
     private static final List<String> RESERVED_KEY_SET = Arrays.asList(BIRTHDAY, EMAIL, FIRSTNAME,
             LASTNAME, GENDER, PHONE, ADDRESS);
 
+    private static final String RUDDER_LABEL = "rudder_id";
+
     // Braze instance
     private Braze braze;
 
@@ -148,7 +150,7 @@ public class BrazeIntegrationFactory extends RudderIntegration<Braze> {
     public static final Factory FACTORY = new Factory() {
         @Override
         public RudderIntegration<?> create(Object settings, RudderClient client, RudderConfig rudderConfig) {
-            return new BrazeIntegrationFactory(settings, rudderConfig);
+            return new BrazeIntegrationFactory(settings, client, rudderConfig);
         }
 
         @Override
@@ -157,7 +159,7 @@ public class BrazeIntegrationFactory extends RudderIntegration<Braze> {
         }
     };
 
-    private BrazeIntegrationFactory(Object config, RudderConfig rudderConfig) {
+    private BrazeIntegrationFactory(Object config, RudderClient client, RudderConfig rudderConfig) {
         String apiKey = "";
         Map<String, Object> destinationConfig = (Map<String, Object>) config;
         if (destinationConfig == null) {
@@ -253,6 +255,7 @@ public class BrazeIntegrationFactory extends RudderIntegration<Braze> {
             );
             Braze.configure(RudderClient.getApplication().getApplicationContext(), builder.build());
             this.braze = Braze.getInstance(RudderClient.getApplication());
+            setUserAlias(client.getAnonymousId());
             RudderLogger.logInfo("Configured Braze + Rudder integration and initialized Braze.");
 
             RudderClient.getApplication().registerActivityLifecycleCallbacks(new Application.ActivityLifecycleCallbacks() {
@@ -300,6 +303,10 @@ public class BrazeIntegrationFactory extends RudderIntegration<Braze> {
                 }
             });
         }
+    }
+
+    private void setUserAlias(String anonymousId) {
+        this.braze.getCurrentUser().addAlias(anonymousId, RUDDER_LABEL);
     }
 
     private void processTrackEvent(RudderMessage element) {
