cc1faab8ccbc161ed40d478395fd279195cb12bc
Vishal Gupta <vguptakota1998@gmail.com>
2025-04-24 14:29:35 +0530

    feat!: update braze sdk version to 35.0.0 (#36)
    * feat: update braze sdk version to 35.0.0
    
    * chore: remove api key and data plane url
    
    * chore: remove unused api key and data plane url from MainApplication class
    
    * chore: downgrade gradle version
    
    * chore: add lint baseline
    
    * chore: update mvn-publish gradle
    
    * chore: add lint-baseline.xml in braze build.gradle
    
    * chore: downgrade compileSdk to 33
    
    * chore: revert gradle and lint changes

--- sample-kotlin/src/main/java/com/rudderlabs/android/sample/kotlin/MainApplication.kt
+++ sample-kotlin/src/main/java/com/rudderlabs/android/sample/kotlin/MainApplication.kt
@@ -1,7 +1,6 @@
 package com.rudderlabs.android.sample.kotlin
 
 import android.app.Application
-import com.rudderlabs.android.sample.kotlin.BuildConfig
 import com.rudderstack.android.integration.braze.BrazeIntegrationFactory
 import com.rudderstack.android.sdk.core.RudderClient
 import com.rudderstack.android.sdk.core.RudderConfig
@@ -10,8 +9,6 @@ import com.rudderstack.android.sdk.core.RudderLogger
 class MainApplication : Application() {
     companion object {
         lateinit var rudderClient: RudderClient
-        const val WRITE_KEY = "2NHVCL9JbmZGFA3RTNZf7hh0j7k"
-        const val DATA_PLANE_URL = "https://rudderstacz.dataplane.rudderstack.com"
     }
 
     override fun onCreate() {
